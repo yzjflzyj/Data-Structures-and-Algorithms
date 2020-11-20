11c3aa1900d2872b3be2c731e755de5646ec43f6
yzjflyzj <yanzhijie2014@163.com>
2020-11-20 19:20:50 +0800

    两有序数组找中位数

--- src/main/java/com/example/test/quiz/Test_2.java
+++ src/main/java/com/example/test/quiz/Test_2.java
@@ -18,16 +18,13 @@ public class Test_2 {
     输出 5。*/
 
     public static void main(String[] args) {
-        /*方法一*/
-        method1_1();
 
-        /*方法二*/
-        method1_2();
+        //method1_1();
 
+        //method1_2();
 
+        method2();
 
-
-        
     }
 
     /**
@@ -71,6 +68,73 @@ public class Test_2 {
         System.out.println(len);
         for (int i = 0; i < len; i++) {
             System.out.println(nums[i]);
+        }
+    }
+
+
+    /**
+     * 题目2
+     */
+    private static void method2() {
+        int[] nums1 = {1,2,3,4,5};
+        int[] nums2 = {6,7,8};
+        //默认第一个数组更长或相等
+        int median = getMedian(nums1,0, nums1.length-1, nums2, 0, nums2.length-1);
+        System.out.println(median);
+    }
+
+    public static int getMedian(int[] a, int begina, int enda, int[] b, int beginb, int endb ) {
+        if (enda - begina == 0) {
+            return a[begina] > b[beginb] ? b[beginb] : a[begina];
+        }
+        if (enda - begina == 1) {
+            if (a[begina] < b[beginb]) {
+                return b[beginb] > a[enda] ? a[enda] : b[beginb];
+            } else {
+                return a[begina] < b[endb] ? a[begina] : b[endb];
+            }
+        }
+
+        if (endb - beginb < 2) {
+            if ((endb - beginb == 0) && (enda - begina) % 2 == 0) {
+                //一个数组奇数个，一个数组一个的终止条件
+                int m = b[beginb];
+                int bb = a[(enda + begina) / 2 - 1];
+                int c = a[(enda + begina) / 2];
+                return (m < bb) ? bb : (m < c ? m : c);
+                //一个数组偶数个，一个数组一个的终止条件
+            } else if ((endb - beginb == 0) && (enda - begina) % 2 != 0) {
+                int m = b[beginb];
+                int c = a[(enda + begina) / 2];
+                int d = a[(enda + begina) / 2 + 1];
+                return m < c ? c : (m < d ? m : d);
+                //一个数组奇数个，一个数组两个；一个数组偶数个，一个数组两个的终止条件。
+            } else {
+                int m = b[beginb];
+                int n = b[endb];
+                int bb = a[(enda + begina) / 2 - 1];
+                int c = a[(enda + begina) / 2];
+                int d = a[(enda + begina) / 2 + 1];
+                if (n < bb) {
+                    return bb;
+                } else if (n > bb && n < c) {
+                    return n;
+                } else if (n > c && n < d) {
+                    return m > c ? m : c;
+                } else {
+                    return m < c ? c : (m < d ? m : d);
+                }
+            }
+        } else {
+            int mida = (enda + begina) / 2;
+            int midb = (endb + beginb) / 2;
+            if (a[mida] < b[midb]) {
+                int step = endb - midb;
+                return getMedian(a, begina + step, enda, b, beginb, endb - step);
+            } else {
+                int step = midb - beginb;
+                return getMedian(a, begina, enda - step, b, beginb + step, endb);
+            }
         }
     }
 }
